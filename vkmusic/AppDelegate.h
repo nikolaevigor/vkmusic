afa94c1d52e6ef501a8155d65751d99744460de5
Igor <lowlol@mail.ru>
2015-10-12 00:57:52 +0300

    Initial Commit

---
+++ vkmusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vkmusic
+//
+//  Created by Igor Nikolaev on 12/10/15.
+//  Copyright © 2015 Igor Nikolaev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
