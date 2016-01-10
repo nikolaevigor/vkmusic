a1e2bdf11d7e0f867d361802943f35cb9691b0c1
Igor <lowlol@mail.ru>
2016-01-10 15:08:55 +0300

    Versioning SongsCell

---
+++ vkmusic/SongsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SongsTableViewCell.m
+//  vkmusic
+//
+//  Created by Igor Nikolaev on 01/01/16.
+//  Copyright © 2016 Igor Nikolaev. All rights reserved.
+//
+
+#import "SongsTableViewCell.h"
+
+@implementation SongsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
